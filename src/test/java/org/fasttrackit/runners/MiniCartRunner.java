c16049ac10a5a085728b65c1714f3491c100bc6f
MaierEduard <maiereduard86@gmail.com>
2019-05-30 12:11:20 +0300

    pom changed

---
+++ src/test/java/org/fasttrackit/runners/MiniCartRunner.java
@@ -0,0 +1,16 @@
+package org.fasttrackit.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "org.fasttrackit",
+        features = "src/test/resources/features/mini-cart.feature",
+        tags = {"~@PLM"},
+        plugin = {"html:target/cucumber-html-reports", "json:target/json-reports/miniCart.json"}
+)
+public class MiniCartRunner {
+}

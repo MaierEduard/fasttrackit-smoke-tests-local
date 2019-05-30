530fee6ee295ce8081fa57f6ca292ede3344f044
MaierEduard <maiereduard86@gmail.com>
2019-05-30 10:10:05 +0300

    pom changed

---
+++ src/test/java/org/fasttrackit/runners/CourseRunner.java
@@ -0,0 +1,15 @@
+package org.fasttrackit.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = "org.fasttrackit",
+        features = "src/test/resources/features/course.feature",
+        plugin = {"html:target/cucumber-html-reports", "json:target/json-reports/course.json"})
+
+
+public class CourseRunner {
+}
